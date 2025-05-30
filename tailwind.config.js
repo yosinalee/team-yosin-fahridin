299e3431a5a7a7e359712a30a57862197e2c911f
yosinalee <yosinaleeshirinjonov@gmail.com>
2025-05-30 17:53:20 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {},
+      screens: {
+      'md': '640px',
+    }
+   },
+   plugins: [],
+ }
